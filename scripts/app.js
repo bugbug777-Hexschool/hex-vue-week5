6f3b3a259394071c341429e5e1626e5517633d58
sihle <gubug777@gmail.com>
2022-02-14 08:48:00 +0800

    add get_all_products()

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,28 @@
 /* eslint-disable no-undef */
+const base = 'https://vue3-course-api.hexschool.io'
+const apiPath = 'sihle'
+
 const app = Vue.createApp({
   data () {
     return {
+      products: []
     }
+  },
+  methods: {
+    get_all_products () {
+      const api = `${base}/v2/api/${apiPath}/products`
+      axios
+        .get(api)
+        .then((res) => {
+          this.products = res.data.products
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  },
+  mounted () {
+    this.get_all_products()
   }
 })
 
