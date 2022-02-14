91520d6acc55859ee169561597b28c1ce8076257
sihle <gubug777@gmail.com>
2022-02-14 09:16:04 +0800

    add product modal

--- scripts/app.js
+++ scripts/app.js
@@ -1,29 +1,52 @@
 /* eslint-disable no-undef */
-const base = 'https://vue3-course-api.hexschool.io'
-const apiPath = 'sihle'
+const base = 'https://vue3-course-api.hexschool.io';
+const apiPath = 'sihle';
 
+// Components
+const productModal = {
+  template: '#userProductModal',
+  data () {
+    return {
+      modal: {},
+    };
+  },
+  methods: {
+    open_modal () {
+      this.modal.show();
+    },
+  },
+  mounted () {
+    this.modal = new bootstrap.Modal(this.$refs.modal);
+  },
+};
+
+// App
 const app = Vue.createApp({
   data () {
     return {
-      products: []
-    }
+      products: [],
+    };
   },
   methods: {
     get_all_products () {
-      const api = `${base}/v2/api/${apiPath}/products`
+      const api = `${base}/v2/api/${apiPath}/products`;
       axios
         .get(api)
         .then((res) => {
-          this.products = res.data.products
+          this.products = res.data.products;
         })
         .catch((err) => {
-          console.log(err)
-        })
-    }
+          console.log(err);
+        });
+    },
+    open_modal () {
+      this.$refs.productModal.open_modal();
+    },
   },
   mounted () {
-    this.get_all_products()
-  }
-})
+    this.get_all_products();
+  },
+});
 
-app.mount('#app')
+app.component('product-modal', productModal);
+app.mount('#app');
